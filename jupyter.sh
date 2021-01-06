d71e66266e43f4f3c8e15994d41dd4bb8131665c
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-01-06 17:24:23 +0100

    Fixed in the jupyter launcher script.

--- jupyter.sh
+++ jupyter.sh
@@ -23,6 +23,6 @@ echo -e  "===============================\n"
 # Reduce verbosity and disable Python buffering
 ENV_VARS="PYTHONWARNINGS=ignore TF_CPP_MIN_LOG_LEVEL=3 PYTHONUNBUFFERED=on EXTENDED_TESTING=False LOGLEVEL=$LOGLEVEL"
 
-docker run -p8888:8888 -it timeseria_jupyter_container /bin/bash -c "$ENV_VARS && jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token=''"
+docker run -p8888:8888 -v $PWD:/home/jovyan/notebooks -it timeseria_jupyter_container /bin/bash -c "$ENV_VARS && jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token=''"
 
 
